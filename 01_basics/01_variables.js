3b8aa8a6d3ff9622eded4d579f3f27b14a14eea7
vineetsing-h <94634816+vineetsing-h@users.noreply.github.com>
2025-01-14 19:54:47 +0000

    Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 14452
+let accountEmail= "vineetsingh4040@gmail.com"
+var accountPassword = "991"
+accountCity = "Rewa"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) we are using nodejs, not browser
+
+console.log("Vineet")
+
+let name = "Vineet"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+
+// number
+//bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Vineet")  // string
+console.log(typeof null);     // object
+console.log(typeof undefined); // undefined
+
+
