ca401c840076d7a007739d8324c6da593acbd078
vineetsing-h <94634816+vineetsing-h@users.noreply.github.com>
2025-01-15 17:52:19 +0000

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -38,3 +38,31 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);          // 33
 console.log(typeof(stringNumber));  // string
 
+// ************** Operations *******************
+let str1 = "hello"
+let str2 = "Vineet"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);  // 12
+console.log(1 + "2");  // 12
+console.log("1" +2+3); // 123
+console.log(1+2+"3");  // 33
+console.log(+true);    //  1 
+console.log(+"");      // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);  // 101
+
+
+
+
+
+
+
+
