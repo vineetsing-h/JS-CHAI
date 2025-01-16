419c2d9d1a6b4d0c233b15f544344e3286d606a1
vineetsing-h <94634816+vineetsing-h@users.noreply.github.com>
2025-01-16 18:06:29 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,31 @@
+const name ="Vineet"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  // string Interpolation
+
+const gameName = new String('ESPN-Fifa')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('i'));
+
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4);
+console.log(anotherString);
+
+const newStringOne = "    hitesh     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'));
+
+
