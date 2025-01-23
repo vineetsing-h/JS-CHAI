ab09d7fddda44d40a0150193aa0cb984588f017c
vineetsing-h <94634816+vineetsing-h@users.noreply.github.com>
2025-01-23 18:47:54 +0000

    control flow

---
+++ 04_control_flow.js/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "feb"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
