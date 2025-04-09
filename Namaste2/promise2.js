f67cd9550617fcd227591ed3cf06e2f02c4b38c5
Vineet Singh <94634816+vineetsing-h@users.noreply.github.com>
2025-04-09 07:47:35 +0000

    async await

---
+++ Namaste2/promise2.js
@@ -0,0 +1,45 @@
+const p1 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("Promise Resolved Value!!")   
+    }, 10000);   
+});
+
+const p2 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("Promise Resolved Value!!")   
+    }, 5000);   
+});
+
+
+// Always returns a promise
+/* async function getData() {
+    return p;
+}
+
+const dataPromise = getData();
+
+dataPromise.then((res) => console.log(res));  */
+
+async function handlePromise() {
+    console.log("Hello");
+    
+    
+    const val = await p1; // Await can only be used inside an async function
+    console.log("Namaste JS"); 
+    console.log(val);
+
+    const val2 = await p2; // Await can only be used inside an async function
+    console.log("Namaste Javascript 2"); 
+    console.log(val2);
+    
+}
+
+handlePromise()
+
+// function getData(){
+//     p.then((res) => console.log(res))
+//     console.log("Namaste JS");
+    
+// }
+
+// getData() 
